b33d744390d09ee172b0cf092b77f92e426c180f
Danny Tuppeny <danny@tuppeny.com>
2019-05-23 12:41:13 +0100

    Support spawning LSP diagnotic server along with other

--- src/extension/commands/analyzer.ts
+++ src/extension/commands/analyzer.ts
@@ -1,13 +1,18 @@
 import * as vs from "vscode";
 import { Analyzer } from "../analysis/analyzer";
+import { lspClient } from "../lsp/setup";
 import { openInBrowser } from "../utils";
 
 export class AnalyzerCommands {
-
 	constructor(context: vs.ExtensionContext, analyzer: Analyzer) {
 		context.subscriptions.push(vs.commands.registerCommand("dart.openAnalyzerDiagnostics", async () => {
 			const res = await analyzer.diagnosticGetServerPort();
 			openInBrowser(`http://localhost:${res.port}/`);
+
+			if (lspClient) {
+				const diagServer = await lspClient.sendRequest<{ port: number }>("dart/diagnosticServer");
+				openInBrowser(`http://localhost:${diagServer.port}`);
+			}
 		}));
 	}
 }
